6257858eda7c8b00c19785c08e1fdd8e51c8c139
Khoa <52100969@student.tdtu.edu.vn>
2023-12-25 16:10:17 +0700

    update smart contract and contract address variable

--- contracts/CryptoContract.sol
+++ contracts/CryptoContract.sol
@@ -3,9 +3,9 @@
 pragma solidity 0.8.19;
 
 contract CryptoExchange {
-    address owner;
+    address public owner;
     // Receiver[] public receivers;
-    Receiver receiver;
+    Receiver public receiver;
 
     constructor() {
         owner = msg.sender;
@@ -19,29 +19,7 @@ contract CryptoExchange {
         bool hasReceived;
     }
 
-    // Receiver receiver;
-
-    // function getReceiverIndex(address payable walletAddress) private view returns (uint256)
-    // {
-    //     for (uint256 i = 0; i < receivers.length; i++) {
-    //         if (receivers[i].walletAddress == walletAddress) {
-    //             return i;
-    //         }
-    //     }
-    //     return 9999;
-    // }
-
-    // add receiver to contract
-    // function addReceiver(address payable walletAddress, string memory name, uint256 timestamp, uint256 amount, bool hasReceived) public {
-    //     if (getReceiverIndex(walletAddress) == 9999) {
-    //         receivers.push(Receiver(walletAddress, name, timestamp, amount , hasReceived));
-    //     }
-    // }
-    function getReceiver() public view returns (Receiver memory) {
-        return receiver;
-    }
-
-    function getBalance() public view returns (uint) {
+    function getOwnerBalance() public view returns (uint) {
         return owner.balance;
     }   
 
@@ -55,11 +33,6 @@ contract CryptoExchange {
 
     // function deposit(address payable walletAddress) public payable {
     //     receivers[getReceiverIndex(walletAddress)].amount += msg.value;
-    // }
-
-    // function withdrawToken(address payable walletAddress) public payable {
-    //     Receiver memory tmpReceiver = receivers[getReceiverIndex(walletAddress)];
-    //     tmpReceiver.walletAddress.transfer(tmpReceiver.amount);
     // }
 
     function withdrawToken() public payable {
